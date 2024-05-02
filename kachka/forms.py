740b8092a711c5fea6d6b474757f8c26bf90e6c3
Nurbek333 <nurbekuktamov333@gmail.com>
2024-05-02 18:02:59 +0500

    commit

--- kachka/forms.py
+++ kachka/forms.py
@@ -1,10 +1,6 @@
 from django.forms import ModelForm
 from .models import Contact
-from django import forms
-from django.db import models
-from django.contrib.auth.models import User
 
-from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 class ContactForm(ModelForm):
@@ -13,13 +9,3 @@ class ContactForm(ModelForm):
         fields = ["name","email","phone_number","description"]
 
 
-class ArticleForm(forms.Form):
-    title = forms.CharField(max_length=200)
-    description = forms.CharField(
-     widget=forms.Textarea(attrs={'placeholder': 'Maqola matnini kiriting'}))
-    image = forms.ImageField()
-
-class CommentForm(forms.Form):
-    first_name = forms.CharField(max_length=200)
-    text = forms.CharField(max_length=200)
-    # rating = forms.IntegerField(validators=[MinValueValidator(1),]) 
